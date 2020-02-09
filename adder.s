4911a454f55a5a0c24cc8ab044e520cb83610700
E-Wawrzynek <57082811+E-Wawrzynek@users.noreply.github.com>
2020-02-08 17:27:53 -0700

    Project 1 Part 1

---
+++ adder.s
@@ -0,0 +1,15 @@
+.text
+.equ LEDs, 0xFF200000
+.equ SWITCHES, 0xFF200040
+.global _start
+_start: 
+movia   r2, LEDs #Address of LEDs
+movia   r3, SWITCHES    #Address of switches
+LOOP:
+ldwio   r4, (r3) #Read the state of switches#
+srli	r5, r4, 5 #Shift the switches 5 bits right
+andi	r6, r4, 31 #And the right five bits with 1s and left with 0s
+add 	r7, r5, r6 #Add the two numbers
+stwio   r7, (r2)# Display the state on LEDs
+br LOOP
+.end
